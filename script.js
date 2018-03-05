26d227adccaec121282bdb443dbeaf57bbeb8f64
Scott Wakefield <scott@npclinics.com.au>
2018-03-06 07:06:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say("Hello I'm 300SunsetBot! I understand you will be visiting on September 7. Just type CHAT to continue to see what I can help you with.")
+            return bot.say("Hello")
                 .then(() => 'askName');
         }
     },
@@ -21,18 +21,48 @@ module.exports = new Script({
         receive: (bot, message) => {
             const name = message.text;
             return bot.setProp('name', name)
-                .then(() => bot.say(`Great! I'll call you ${name}
-Is that OK? %[Yes](postback:yes) %[No](postback:no)`))
-                .then(() => 'finish');
+                .then(() => bot.say(`Great! I'll call you ${name}`))
+                .then(() => 'howToHelp');
         }
     },
-
-    finish: {
+    
+    howToHelp: {
+        prompt: (bot) => bot.say("How can I help you today? \n 1) General Question (NA) \n 2) Flu Vaccinations (CA) \n 3) Question for the nurse (RA) \n 4) Live Chat (LC)"),
+        receive: (bot, message) => {
+            const task = message.text;
+            switch(task) {        
+                case "NA":
+                    return bot.setProp('task', task)
+                        .then(() => bot.say(`Great! What's your Question?.`))
+                        .then(() => 'finish');
+                    break;
+                case "CA":
+                    return bot.setProp('task', task)
+                                    .then(() => bot.say(`Great! How can we help today?`))
+                                    .then(() => 'finish');
+                    break;
+                case "RA":
+                    return bot.setProp('task', task)
+                                    .then(() => bot.say(`Great! What's your Question?`))
+                                    .then(() => 'finish');
+                    break;
+                case "LC":
+                    return bot.setProp('task', task)
+                                    .then(() => bot.say(`Great! I'll go fetch a NPCA representative`))
+                                    .then(() => 'finish');
+                    break;
+                default:
+                    return bot.setProp('task', task)
+                                    .then(() => bot.say(`An error occurred`))
+                                    .then(() => 'finish');
+                }            
+        }
+    },
+   finish: {
         receive: (bot, message) => {
             return bot.getProp('name')
-                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
-                        'teach me how to do anything else!'))
-                .then(() => 'finish');
+                .then((name) => bot.say(`Hello again, ${name}!`))
+                .then(() => 'howToHelp');
         }
     }
 });
